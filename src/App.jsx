4f4a3224aa11d2eecbd3e1440d31bb841f442e0f
guyera <thegraygrimm@graymansurvivaltech.com>
2025-08-14 12:16:55 -0400

    Added view all functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 // src/App.jsx
 import { useState, useEffect } from 'react';
 import * as petServices from './services/petService.js';
+import PetList from './components/PetList/PetList.jsx';
 
 const App = () => {
-  const [pets, setpets] = useState([]);
+  const [pets, setPets] = useState([]);
 
-  
-  return <h1>Hello world!</h1>;
+  useEffect(() => {
+    const fetchPets = async () => {
+      try {
+        const fetchedPets = await petServices.index();
+
+        if (fetchedPets.err) {
+          throw new Error(fetchedPets.err);
+        }
+
+        setPets(fetchedPets);
+      } catch (err) {
+        console.log(err);
+      };
+    };
+    fetchPets();
+  }, []);
+
+  return (
+    <>
+      <PetList pets={pets} />
+    </>
+  )
 };
 
 export default App;
